4c2c693809f7e9c50702fbc9c8f3616b8f67c937
Maya Hagan <miyaclessa@gmail.com>
2021-07-10 17:11:44 +1200

    add AboutPizza.jsx

---
+++ src/AboutPizza.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import images3 from './Images/yanya.jpg'
+
+class AboutPizza extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container  about-pizza">
+    <div className="row">
+        <center><h2>About Pizza</h2></center>
+  
+       <div className="col-md-6">
+          <img src={images3} className="img-responsive"/>
+       </div>
+       <div className="col-md-6">
+           <h3>Pizza Is Awesome!</h3>
+           <p>
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+           </p>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat.
+           </p>   
+
+            <p>
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+           </p>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+           </p>
+          </div>
+        </div>
+      </div>
+            </div>
+        );
+    }
+}
+
+export default AboutPizza;
