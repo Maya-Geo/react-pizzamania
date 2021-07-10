23240532c354261ccbadf74702b5750c3bcb7e73
Maya Hagan <miyaclessa@gmail.com>
2021-07-10 17:12:21 +1200

    add Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+    <div className="container">
+        <div className="row">
+      <p>
+          <center>&copy; 2020 Pizzamania All Reserved!</center>
+      </p>
+      </div>
+    </div>
+  </div>
+        </div>
+    );
+}
+
+export default Footer;
